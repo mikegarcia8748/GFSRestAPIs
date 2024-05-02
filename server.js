5afb55fc6d5b6c2d9beed3d1bd7c4da8376e4699
mikegarcia8748 <mikegarcia8748@gmail.com>
2024-05-02 10:56:27 +0800

    remove the IP address of node server

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const http = require('http');
 const app = require('./app');
 
 const PORT = process.env.NODE_SERVER_PORT;
-const IP_ADDRESS = process.env.IP_ADDRESS;
+// const IP_ADDRESS = process.env.IP_ADDRESS;
 
 const server = http.createServer(app)
 
-server.listen(PORT, IP_ADDRESS)
+server.listen(PORT)
 
