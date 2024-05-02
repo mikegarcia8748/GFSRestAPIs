7a3e689d83b291e124162c330bf94d0a82a0c7df
mikegarcia8748 <mikegarcia8748@gmail.com>
2024-05-02 15:38:32 +0800

    Added deploy.sh file for deployment script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Change to your project directory
+cd /path/to/your/nodejs/project
+
+# Pull the latest changes from the main branch of your GitHub repository
+git pull origin main
+
+# Install or update dependencies
+npm install
+
+npm start
